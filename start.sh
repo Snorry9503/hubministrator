0439e9d5c3a0b820d2cdabcbdcaf1eaf260f6397
lorenzo-giacomini <l.giacomini@greenvulcano.com>
2018-03-16 17:35:59 +0100

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
+pip install virtualenv
+pip install virtualenvwrapper
+export WORKON_HOME=~/Envs
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv my-venv
 workon my-venv
- export FLASK_CONFIG=development
- export FLASK_APP=run.py
+export FLASK_CONFIG=development
+export FLASK_APP=run.py
 
 rm instance/config.py
 touch istance/config.py
