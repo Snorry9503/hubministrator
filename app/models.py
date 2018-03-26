e8d03a4ae5fe65b7327f5aba3cf9c2a1472f3780
Snorry9503 <gabrielerosato869@gmail.com>
2018-03-26 12:14:36 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,10 +5,164 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login_manager
 
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+
+class Validation(db.Model):
+
+    __tablename__ = 'validations'
+    id_chd = db.Column(db.Integer, primary_key=True)
+    val_flag = db.Column(db.Boolean, default=False)
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id_usr = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(40), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    is_providers = db.Column(db.Boolean, default=False)
+    is_resident = db.Column(db.Boolean, default=False)
+    resident_fk = db.relationship('Resident_Registry', backref='user',
+                                lazy='dynamic')
+    administrator_fk = db.relationship('Administrator_Registry', backref='user',
+                                lazy='dynamic')
+    provider_fk = db.relationship('Provider_Registry', backref='user',
+                                lazy='dynamic')
+
+
+class Resident_Registry(db.Model):
+    """
+    Crea una tabella anagrafica condomini
+    """
+
+    __tablename__ = 'residents_registry'
+    id_resident = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    surname = db.Column(db.String(40), nullable=False)
+    cf = db.Column(db.String(16), nullable=False)
+    sex = db.Column(db.Boolean, default='M')
+    birthdate = db.Column(db.Date, nullable=False)
+    birthplace = db.Column(db.String(20), nullable=False)
+    identity_card = db.Column(db.String(8), nullable=False)
+    address = db.Column(db.String(40), nullable=False)
+    residence = db.Column(db.String(40), nullable=False)
+    phone_home = db.Column(db.String(20), nullable=False)
+    phone_mobile = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(40), nullable=False)
+    flat = db.Column(db.String(2), nullable=False)
+    thousandths = db.Column(db.Float, nullable=False)
+    id_usr = db.Column(db.Integer, db.ForeignKey('users.id_usr'))
+
+
+class Administrator_Registry(db.Model):
+
+    __tablename__ = 'administrators_registry'
+    id_admin = db.Column(db.Integer, primary_key=True)
+    license_n = db.Column(db.String(20), nullable=False)
+    p_iva = db.Column(db.String(13), nullable=False)
+    condo_own_n = db.Column(db.Integer, nullable=False)
+    id_usr = db.Column(db.Integer, db.ForeignKey('users.id_usr'))
+
+    condo_fk = db.relationship('Condo', backref='administrators_registry',
+                                lazy='dynamic')
+
+
+class Provider_Registry(db.Model):
+
+    __tablename__ = 'providers_registry'
+
+    id_provider = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(40), nullable=False)
+    pi_cf = db.Column(db.String(16), nullable=False)
+    city = db.Column(db.String(40), nullable=False)
+    district = db.Column(db.String(40), nullable=False)
+    manager = db.Column(db.String(30))
+    phone_home = db.Column(db.String(20), nullable=False)
+    phone_mobile = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(40), nullable=False)
+    site = db.Column(db.String(40))
+    category = db.Column(db.Integer, nullable=False)
+    id_usr = db.Column(db.Integer, db.ForeignKey('users.id_usr'))
+
+    provider_fk = db.relationship('Provider_Category', backref='providers_registry',
+                                lazy='dynamic')
+
+
+class Provider_Category(db.Model):
+
+    __tablename__ = 'providers_category'
+
+    id_provider = db.Column(db.Integer, db.ForeignKey('providers_registry.id_provider'),  primary_key=True)
+    boiler = db.Column(db.Integer, nullable=False)
+    lifter = db.Column(db.Integer, nullable=False)
+    extinguisher = db.Column(db.Integer, nullable=False)
+    gas = db.Column(db.Integer, nullable=False)
+    light = db.Column(db.Integer, nullable=False)
+    cleaning = db.Column(db.Integer, nullable=False)
+    electrician = db.Column(db.Integer, nullable=False)
+    plumber = db.Column(db.Integer, nullable=False)
+    lawyer = db.Column(db.Integer, nullable=False)
+    achitect = db.Column(db.Integer, nullable=False)
+    antenna = db.Column(db.Integer, nullable=False)
+    sewer = db.Column(db.Integer, nullable=False)
+    business_consultant = db.Column(db.Integer, nullable=False)
+    exterminator = db.Column(db.Integer, nullable=False)
+    bricklayer = db.Column(db.Integer, nullable=False)
+    gardeners = db.Column(db.Integer, nullable=False)
+
+
+class Condo(db.Model):
+
+    __tablename__ = 'condos'
+    id_condo = db.Column(db.Integer, primary_key=True)
+
+    condo_name = db.Column(db.String(30), nullable=False)
+    id_admin = db.Column(db.Integer, db.ForeignKey('administrators_registry.id_admin'))
+
+    condo_data_fk = db.relationship('Condo', backref='condo',
+                                lazy='dynamic')
+
+
+class Condos_Data(db.Model):
+
+    __tablename__ = 'condos_data'
+    condo_name = db.Column(db.String(30))
+    condo_age = db.Column(db.Date, nullable=False)
+    cf_c = db.Column(db.String(16), nullable=False)
+    city = db.Column(db.String(20), nullable=False)
+    district = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.String(30), nullable=False)
+    house_n = db.Column(db.Integer)
+    condo_n = db.Column(db.Integer)
+    stair_n = db.Column(db.Integer)
+    flat_n = db.Column(db.Integer)
+    cap = db.Column(db.Integer)
+    id_condo = db.Column(db.Integer, db.ForeignKey('condos.id_condo'), primary_key=True)
+
+
+    @property
+    def password(self):
+
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<USER: {}>'.format(self.username)
+
+#inizio debug
+
+
 class Employee(UserMixin, db.Model):
-    """
-    Create an Employee table
-    """
 
     # Ensures table will be named in plural and not in singular
     # as is the name of the model
@@ -26,22 +180,13 @@ class Employee(UserMixin, db.Model):
 
     @property
     def password(self):
-        """
-        Prevent pasword from being accessed
-        """
         raise AttributeError('password is not a readable attribute.')
 
     @password.setter
     def password(self, password):
-        """
-        Set password to a hashed password
-        """
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-        """
-        Check if hashed password matches actual password
-        """
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
@@ -53,9 +198,6 @@ def load_user(user_id):
     return Employee.query.get(int(user_id))
 
 class Department(db.Model):
-    """
-    Create a Department table
-    """
 
     __tablename__ = 'departments'
 
@@ -69,9 +211,6 @@ class Department(db.Model):
         return '<Department: {}>'.format(self.name)
 
 class Role(db.Model):
-    """
-    Create a Role table
-    """
 
     __tablename__ = 'roles'
 
